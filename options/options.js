3229651d6eb012fc586fcb31851892135329506e
Crowdbotics <team@crowdbotics.com>
2022-06-03 04:11:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "money_vault_35584",
+  url: "https://money_vault_35584.botics.co",
+  api: "https://money_vault_35584.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
